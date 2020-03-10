8137477f517b41ed2e8b8dc0d47b3da34393106a
Renato Gonçalves <renatoslip@hotmail.com>
2020-03-10 11:06:44 -0300

    consulta bruta Raw

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,19 +43,60 @@ class UserController extends Controller
          *
          */
 
-        $users = DB::table('users')
-                        ->select('users.id', 'users.name', 'users.status')
-                        ->where('users.status', '=', '1')
-                        ->orderBy('name', 'DESC')
-                        //->oldest('name') // ASC
-                        //->latest('name') // DESC
-                        //->inRandomOrder()
-                        ->limit(10)
-                        ->offset(10)
-                        ->get();
+        //        $users = DB::table('users')
+        //                        ->select('users.id', 'users.name', 'users.status')
+        //                        ->where('users.status', '=', '1')
+        //                        ->orderBy('name', 'DESC')
+        //                        //->oldest('name') // ASC
+        //                        //->latest('name') // DESC
+        //                        //->inRandomOrder()
+        //                        ->limit(10)
+        //                        ->offset(10)
+        //                        ->get();
+        //
+        //        foreach($users as $user){
+        //            echo "#{$user->id} Nome: {$user->name} {$user->status}<br>";
+        //        }
 
-        foreach($users as $user){
-            echo "#{$user->id} Nome: {$user->name} {$user->status}<br>";
+        /**
+         * selectRaw(exp)
+         *      Informa quais campos devem ser retornados através de uma expressão
+         *
+         * whereRaw(exp)
+         *      Filtra os registros que obedecem a expressão SQL
+         *
+         * orderByRaw(exp)
+         *      Permite que seja feita expressão na ordenação
+         */
+
+        //        $users = DB::table('users')
+        //                    ->selectRaw('users.id, users.name, CASE WHEN users.status = 1 THEN "ATIVO" ELSE "INATIVO" END status_description')
+        //                    ->whereRaw('(SELECT COUNT(1) FROM addresses a WHERE a.user = users.id) > 2')
+        //                    ->whereRaw('users.status = 1')
+        //                    ->orderByRaw('updated_at - created_at', 'ASC')
+        //                    ->get();
+
+        /**
+         * raw(exp)
+         *      Nível mais baixo de expressão bruta, onde você pode informar a query completa
+         */
+        
+        $users = DB::select(DB::raw('SELECT
+                                    users.id,
+                                    users.name,
+
+                                    CASE
+                                        WHEN users.status = 1 THEN "ATIVO"
+                                        ELSE "INATIVO"
+                                    END status_description
+
+                                    FROM users
+                                    WHERE (SELECT COUNT(1) FROM addresses a WHERE a.user = users.id) > 2
+                                        AND users.status = :userStatus
+                                    ORDER BY updated_at - created_at ASC;'), ['userStatus' => '1']);
+
+        foreach ($users as $user) {
+            echo "#{$user->id} Nome: {$user->name} {$user->status_description}<br>";
         }
 
     }
