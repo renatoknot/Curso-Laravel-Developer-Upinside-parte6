dd738be96147be398ebf123fdedcd7d113fe2e8a
Renato Gonçalves <renatoslip@hotmail.com>
2020-03-10 15:27:09 -0300

    cadastro atualização deleção

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -179,21 +179,32 @@ class UserController extends Controller
          *      INNER JOIN table ON (field_1 (operator) field_2 AND field_3 (operator_2) field_4)
          */
         
-        $users = DB::table('users')
-                    ->select('users.id', 'users.name', 'users.status', 'addresses.address')
-                    //->leftJoin('addresses', 'users.id', '=', 'addresses.user')
-                    ->join('addresses', function($join){
-                        $join->on('users.id', '=', 'addresses.user')
-                            ->where('addresses.status', '=', '1');
-                    })
-                    ->orderby('users.id', 'ASC')
-                    ->get();
+        // $users = DB::table('users')
+        //             ->select('users.id', 'users.name', 'users.status', 'addresses.address')
+        //             //->leftJoin('addresses', 'users.id', '=', 'addresses.user')
+        //             ->join('addresses', function($join){
+        //                 $join->on('users.id', '=', 'addresses.user')
+        //                     ->where('addresses.status', '=', '1');
+        //             })
+        //             ->orderby('users.id', 'ASC')
+        //             ->get();
 
-        echo "Total de registros: {$users->count()}<br>";
+        // echo "Total de registros: {$users->count()}<br>";
 
-        foreach ($users as $user) {
-            echo "#{$user->id} Nome: {$user->name} {$user->status} {$user->address}<br>";
-        }
+        // foreach ($users as $user) {
+        //     echo "#{$user->id} Nome: {$user->name} {$user->status} {$user->address}<br>";
+        // }
+        
+        // DB::table('users')->insert([
+        //     'name' => 'Gustavo Web',
+        //     'email' => 'cursos@upinside.com.br',
+        //     'password' => bcrypt('123456'),
+        //     'status' => 1
+        // ]);
 
+        // DB::table('users')->where('id', '=', 1001)->update([
+        //     'email' => 'gustavo@upinside.com.br'
+        // ]);
+        DB::table('users')->where('id', '=', 1001)->delete();
     }
 }
